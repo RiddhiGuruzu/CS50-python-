88374a6fe0d48162cd1f25e3876814a4f82f9bff
RiddhiGuruzu <95412700+RiddhiGuruzu@users.noreply.github.com>
2026-06-26 07:09:16 +0530

    Create playback.py

---
+++ week1/exercises/playback.py
@@ -0,0 +1,5 @@
+hi=input("Enter:").split()
+print(*hi,sep="...")
+
+#The astrisk is used to unpack the list and print each element separated by the specified separator, which in this case is "...".
+#Documentation: print(*objects, sep=' ', end='\n', file=None, flush=False)
